75d39f88dd7d6d6a77c63e66f6b7277447fc3cd1
BRIAN-DESKTOP\Brian <brianlee1200@gmail.com>
2023-04-20 23:01:47 -0500

    add poisoned mind alert and color border

--- SanitysEdgeHelper/SanitysEdgeHelperData.lua
+++ SanitysEdgeHelper/SanitysEdgeHelperData.lua
@@ -16,11 +16,13 @@ SEH.data    = {
   yaseyla_firebombs_preexecute_cd = 24, -- how often it can cast firebombs pre-execute
   yaseyla_firebombs_execute_cd = 12, -- how often it can cast firebombs execute
 
+  -- Ansuul
   ansuul_sunburst = 199344,
   ansuul_wrack = 184621,
   ansuul_wrathstorm = 198759,
   ansuul_calamity = 186728,
   ansuul_execute = 198797,
+  ansuul_poisoned_mind = 184710,
 
   hindered_effect = 165972,
   
--- SanitysEdgeHelper/boss/Ansuul.lua
+++ SanitysEdgeHelper/boss/Ansuul.lua
@@ -1,3 +1,10 @@
 SEH = SEH or {}
 local SEH = SEH
 SEH.Ansuul = {}
+
+function SEH.Ansuul.Poisoned_Mind(result, targetType, targetUnitId, hitValue)
+  if result == ACTION_RESULT_EFFECT_GAINED_DURATION and targetType == COMBAT_UNIT_TYPE_PLAYER then
+    SEH.Alert("", "Poisoned Mind (self)", 0x23B402FF, SEH.data.ansuul_poisoned_mind, SOUNDS.OBJECTIVE_DISCOVERED, 2000)
+    CombatAlerts.AlertBorder(true, hitValue, "green")
+  end
+end
